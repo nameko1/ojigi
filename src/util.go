4ebdffed9fa018b1432963f32b70318201e95440
なめこ <nameko@nameko.local>
2018-03-31 02:42:58 +0900

    [test] utilのテスト用にpasswordのファイルパスを指定できるようにした

--- src/util.go
+++ src/util.go
@@ -1,4 +1,5 @@
 package ojigi
+
 import (
     "os"
     "bufio"
@@ -6,25 +7,34 @@ import (
     "fmt"
 )
 
-const DirPath = "/etc/ojigi"
-const FilePath = DirPath+"/passwd"
-const NewFilePath = DirPath+"/newpasswd"
+const (
+DirPath = "/etc/ojigi"
+FilePath = DirPath+"/ojigi_note"
+PasswdPath = DirPath+"/passwd"
+NewFilePath = DirPath+"/new_ojigi_note"
+)
 
-func GetPasswdFromService(service string) string {
-    file, err := os.OpenFile(FilePath, os.O_RDONLY|os.O_CREATE, 0600)
+func GetPasswdFromService(service string, optionalPath ...string) string {
+    path := FilePath
+    if len(optionalPath) > 0 {
+        path = optionalPath[0]
+    }
+
+    file, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0600)
     if err != nil {
-        fmt.Println(err) 
+        fmt.Println(err)
+        return ""
     }
     defer file.Close()
 
     sc := bufio.NewScanner(file)
     for i := 1; sc.Scan(); i++ {
         if err := sc.Err(); err != nil {
-            break 
+            return ""
         }
         line := strings.Split(sc.Text(), ":")
         if line[0] == service {
-            return line[1] 
+            return line[1]
         }
     }
     return ""
