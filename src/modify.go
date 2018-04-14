0262283889fa5ea30b88a87a41552c9b60095057
なめこ <nameko@nameko.local>
2018-04-14 22:52:55 +0900

    [fix] modify,delete時に成功・失敗を知らせるようにした modify時にもpassword lengthを記録するように変更

--- src/modify.go
+++ src/modify.go
@@ -5,10 +5,12 @@ import (
     "fmt"
     "bufio"
     "strings"
+    "strconv"
 )
 
 func Modify(service string, passwd []byte, key []byte, action string) {
 
+    complate := false
     oldFile, oldFileErr := os.OpenFile(FilePath, os.O_RDONLY|os.O_CREATE, 0600)
     if oldFileErr != nil {
         fmt.Println("Faild: can not open password file")
@@ -24,8 +26,6 @@ func Modify(service string, passwd []byte, key []byte, action string) {
     defer oldFile.Close()
     defer newFile.Close()
 
-    cipherPasswd := EncodePasswd(passwd, key)
-
     sc := bufio.NewScanner(oldFile)
     for i := 1; sc.Scan(); i++ {
         if err := sc.Err(); err != nil {
@@ -35,8 +35,13 @@ func Modify(service string, passwd []byte, key []byte, action string) {
         }
         if l := strings.Split(sc.Text(), ":"); l[0] == service {
             if action == "modify" {
-                newFile.Write([]byte(l[0]+":"+cipherPasswd+"\n"))
+                if len(passwd) == 0 {
+                    passwd = VerifyPasswdScanf("Enter service password: ", "Verify: ", faildPassword)
+                }
+                cipherPasswd := EncodePasswd(passwd, key)
+                newFile.Write([]byte(l[0]+":"+strconv.Itoa(len(passwd))+":"+cipherPasswd+"\n"))
             }
+            complate = true
         } else {
             newFile.Write([]byte(sc.Text()+"\n"))
         }
@@ -44,4 +49,9 @@ func Modify(service string, passwd []byte, key []byte, action string) {
 
     os.Remove(FilePath)
     os.Rename(NewFilePath, FilePath)
+    if complate {
+        fmt.Printf("Success to %s password\n", action)
+    } else {
+        fmt.Printf("Fail to %s, password not found\n", action)
+    }
 }
